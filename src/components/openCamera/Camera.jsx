5768a591bae4d0386bae8dfa173140601e740d90
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-01 14:27:02 +0530

    Update Camera.jsx

--- src/components/openCamera/Camera.jsx
+++ src/components/openCamera/Camera.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 
 import "./Camera.css";
+
 function Camera() {
   const [stream, setStream] = useState(null);
   const videoRef = useRef(null);
