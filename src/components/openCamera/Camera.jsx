6aa10d6fab205f59eb696658b4c066f61135d2b6
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-01 14:26:05 +0530

    Update Camera.jsx

--- src/components/openCamera/Camera.jsx
+++ src/components/openCamera/Camera.jsx
@@ -103,7 +103,7 @@ function Camera() {
 
       console.log("captured...!");
 
-      fetch("http://127.0.0.1:8000/image", { // TODO: BUG
+      fetch("http://127.0.0.1:8000/image", { 
         method: "POST",
         headers: {
           "Content-Type": "application/json",
