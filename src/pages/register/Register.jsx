fac3794e0a3ba2569255fc28899f73f8802ba8a4
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-04 12:33:13 +0530

    Update Register.jsx

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -33,7 +33,7 @@ export default function Register() {
 
       try {
         console.log("registering user");
-        await axios.post("http://127.0.0.1:8000/api/auth/register", user);
+        await axios.post("http://127.0.0.1:8000/api/auth/register", user, config);
         navigate("/login");
       } catch (error) {
         console.log(error);
