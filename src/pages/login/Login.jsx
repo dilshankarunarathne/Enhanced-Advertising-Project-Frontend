42a6b8bd405ca193de1710719f52477858d8b6da
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-04 11:46:41 +0530

    Update Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -23,7 +23,7 @@ export default function Login() {
         'Content-Type': 'application/json',
       },
     }
-    body: JSON.stringify({
+    body: JSON.stringify({ email: emailValue, password: passwordValue })
     )
   };
 
