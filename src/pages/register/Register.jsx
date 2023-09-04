d96c83f347b126cbf0ff8dbee166d02043ddba94
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-04 12:50:42 +0530

    Update Register.jsx

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -24,7 +24,7 @@ export default function Register() {
 
       const config = {
         headers: {
-          "Content-Type": "application/json",
+          "Content-Type": "multipart/form-data",
           "Access-Control-Allow-Origin": "*",
           "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS"
         }
