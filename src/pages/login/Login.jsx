e36b03b01cbfd56dc91d563e76b4b3394fc5ac49
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-04 11:46:01 +0530

    Update Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -20,6 +20,7 @@ export default function Login() {
       'http://127.0.0.1:8000/api/auth/login', {
       method: 'POST',
       headers: {
+        'Content-Type': 'application/json',
     })
   };
 
