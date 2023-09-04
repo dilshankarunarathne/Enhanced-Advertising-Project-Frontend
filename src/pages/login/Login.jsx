7ce9be2423007da0332525b201b843dbb4cf011f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-04 12:06:17 +0530

    Update Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -28,6 +28,8 @@ export default function Login() {
     if (response.ok) {  // successful login
       // TODO: set token in local storage
       const token = response.json().token;
+
+      // store the token
       
 
       navigate("/");
