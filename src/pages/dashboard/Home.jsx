231dd1fa449cdc4bffdd0073737547c36baf9f57
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-04 12:22:33 +0530

    Update Home.jsx

--- src/pages/dashboard/Home.jsx
+++ src/pages/dashboard/Home.jsx
@@ -11,7 +11,7 @@ const withAuth = (Component) => {
     const isAuthenticated = localStorage.getItem("token");
 
     if (!isAuthenticated) {
-      return <Navigate to="/login" replace />;
+      return <Navigate to="/login" />;
     }
 
     return <Component />;
