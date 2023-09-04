7a67b9c8d39e001ee1e3d084e78d42fb2afd3c22
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-04 12:10:04 +0530

    Update Camera.jsx

--- src/components/openCamera/Camera.jsx
+++ src/components/openCamera/Camera.jsx
@@ -45,13 +45,14 @@ function Camera() {
 
   useEffect(() => {
     let intervalId;
-    
+
     if (stream) {
       intervalId = setInterval(() => {
         const canvas = document.createElement("canvas");
         canvas.width = videoRef.current.videoWidth;
         canvas.height = videoRef.current.videoHeight;
         canvas.getContext("2d").drawImage(videoRef.current, 0, 0);
+        
         canvas.toBlob((blob) => {
           const formData = new FormData();
           formData.append("image", blob, "image.jpg");
