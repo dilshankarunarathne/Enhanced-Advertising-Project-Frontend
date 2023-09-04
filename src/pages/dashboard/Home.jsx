3011efacef265928b3ff0852f32348d00a62d74b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-04 12:20:38 +0530

    Update Home.jsx

--- src/pages/dashboard/Home.jsx
+++ src/pages/dashboard/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Camera from "../../components/openCamera/Camera";
-import { Redirect } from "react-router-dom";
+
+import { Redirect } from 'react-router-dom';
 
 import "./home.css";
 
