d1b6bff982ce336f31d245020842bfcd2e9be41e
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-04 11:28:13 +0530

    Update Home.jsx

--- src/pages/dashboard/Home.jsx
+++ src/pages/dashboard/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Camera from "../../components/openCamera/Camera";
+
 import "./home.css";
 export const Home = () => {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
