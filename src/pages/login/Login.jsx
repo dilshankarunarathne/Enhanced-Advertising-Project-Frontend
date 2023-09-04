77cc306889e7cb8cbb31778c1ed987d493ac3a35
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-04 13:00:33 +0530

    Update Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -17,7 +17,6 @@ export default function Login() {
     formData.append('username', usernameValue);
     formData.append('password', passwordValue);
     
-
     const response = await fetch(
       'http://127.0.0.1:8000/api/auth/login', {
       method: 'POST',
