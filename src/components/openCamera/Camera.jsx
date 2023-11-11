bc552a500c5b0be86d4ad112978078ba8aaa1604
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-11 11:44:41 +0530

    Update Camera.jsx

--- src/components/openCamera/Camera.jsx
+++ src/components/openCamera/Camera.jsx
@@ -3,22 +3,16 @@ import "./Camera.css";
 
 function Camera(props) {
   const [stream, setStream] = useState(null);
+  const [socket, setSocket] = useState(null);
   const videoRef = useRef(null);
 
   useEffect(() => {
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then((stream) => {
-          setStream(stream);
-          if (videoRef.current) {
-            videoRef.current.srcObject = stream;
-          }
-        })
-        .catch((error) => {
-          console.error("Error accessing camera", error);
-        });
-    }
+    const socket = new WebSocket('ws://localhost:8000/ws/video_stream/');
+    setSocket(socket);
+
+    return () => {
+      socket.close();
+    };
   }, []);
 
   const handleButtonClick = () => {
@@ -50,32 +44,14 @@ function Camera(props) {
     canvas.getContext("2d").drawImage(videoRef.current, 0, 0);
 
     canvas.toBlob((blob) => {
-      const formData = new FormData();
-      formData.append("image", blob, "image.jpg");
-    
-      fetch("http://127.0.0.1:8000/api/image/evaluate", {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-        body: formData,
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          return response.text();
-        })
-        .then((data) => {
-          console.log(data);
-          
-          const [ageRange, gender, interest, imgUrl] = JSON.parse(data);
-
-          props.updateAgeAndGender(ageRange, gender, imgUrl, interest);
-        })
-        .catch((error) => {
-          console.error("Error sending image", error);
-        });
+      const reader = new FileReader();
+      reader.onloadend = function() {
+        const base64data = reader.result;
+        if (socket.readyState === WebSocket.OPEN) {
+          socket.send(base64data);
+        }
+      }
+      reader.readAsDataURL(blob);
     }, "image/jpeg", 0.9);
   }
 
