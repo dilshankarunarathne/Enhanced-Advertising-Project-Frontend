f039cb9f2f57e7e06c2177d7107d82e9ec49848c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-20 15:47:38 +0530

    charts done

--- src/pages/report/Report.jsx
+++ src/pages/report/Report.jsx
@@ -3,9 +3,56 @@ import Navbar from "../../components/navbar/Navbar";
 import { Form, Table } from 'react-bootstrap';
 import axios from 'axios';
 
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+import './Report.css';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 export const Report = () => {
   const [data, setData] = useState([]);
   const [selectedMonth, setSelectedMonth] = useState('november');
+
+  const maleData = {
+    labels: ['4-12', '13-26', '27-40', 'above 40'],
+    datasets: [
+      {
+        label: '# of uses',
+        data: [data.male_4_12, data.male_13_26, data.male_27_40, data.male_above_40],
+        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#FF6384'],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const femaleData = {
+    labels: ['4-12', '13-26', '27-40', 'above 40'],
+    datasets: [
+      {
+        label: '# of uses',
+        data: [data.female_4_12, data.female_13_26, data.female_27_40, data.female_above_40],
+        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#FF6384'],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
 
   useEffect(() => {
     axios.get(`http://localhost:8000/api/report?month=${selectedMonth}`)
@@ -80,6 +127,11 @@ export const Report = () => {
             </tr>
           </tbody>
         </Table>
+      </div>
+
+      <div className="chart">
+        <Pie className="pie" data={maleData} />
+        <Pie className="pie" data={femaleData} />
       </div>
     </div>
   );
